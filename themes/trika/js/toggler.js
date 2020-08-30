0bcd0f141b45c2420f5358ca338cdb426fd08982
Davay <davay@frkt.ru>
2020-08-30 23:12:29 +0300

    no-buttons

--- themes/trika/js/toggler.js
+++ themes/trika/js/toggler.js
@@ -3,12 +3,12 @@ window.addEventListener('DOMContentLoaded', () => {
     let toggles = document.getElementsByClassName('toggle')
     let content = document.getElementById('content')
     for (let toggle of toggles) {
-      let type = toggle.getAttribute('data-type')
-      
-      toggle.onclick = () => {
-        toggle.classList.toggle('off')
-        content.classList.toggle(type)
-      }
+        let type = toggle.getAttribute('data-type')
+
+        toggle.onclick = () => {
+            toggle.classList.toggle('off')
+            content.classList.toggle('no-' + type)
+        }
     }
 
 })
